a9c56d873eccc343167a5fc1ebd41d34bdf4a906
Jordan Hemphill <hemphillmusic@gmail.com>
2025-06-01 15:18:07 -0500

    first draft

---
+++ script.js
@@ -0,0 +1,23 @@
+  window.addEventListener("scroll", function () {
+    const header = document.getElementById("site-header");
+    if (window.scrollY > 0) {
+      header.classList.add("header-scrolled");
+    } else {
+      header.classList.remove("header-scrolled");
+    }
+  });
+
+
+
+
+  function toggleMobileMenu(menu){
+  menu.classList.toggle('open');
+}
+
+function clearPlaceholder(input) {
+  input.placeholder = '';
+}
+
+function toHeadshot(){
+  window.location.href = "headshots.html"
+}
